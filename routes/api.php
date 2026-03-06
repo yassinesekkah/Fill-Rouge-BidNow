270395b2b989622bfd78385293679375612e1792
Yassine Sekkah <yassinesekkah11@gmail.com>
2026-03-06 17:27:58 +0000

    feat: implement API authentication using Sanctum (register, login, protected route)

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
-
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Http\Request;
 
 Route::get('/test', function () {
     return response()->json([
@@ -11,3 +12,12 @@ Route::get('/test', function () {
 });
 
 Route::get('/products', [ProductController::class, 'index']);
+
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
+
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class,'logout']);
+
+Route::middleware('auth:sanctum')->get('/profile', function (Request $request) {
+    return $request->user();
+});
